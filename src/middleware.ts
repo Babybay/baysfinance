6c04af4c81274b58d5ec329207b3a768457701db
Babybay <satriab2108@gmail.com>
2026-02-23 22:38:23 +0800

    using prisma

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ const isPublicRoute = createRouteMatcher([
     '/',
     '/sign-in(.*)',
     '/sign-up(.*)',
-    '/api/webhooks(.*)',  // Clerk webhooks must be public
+    '/api/webhooks(.*)',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
